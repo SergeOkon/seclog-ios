a2c622db38b3a94c369e26b03e8c55e3dda2e8e5
Serge <work@serge-mb-2.local>
2019-10-16 11:36:34 -0700

    initial commit. Created lib with 'pod lib create'

---
+++ Example/Pods/Target Support Files/Pods-SecLog_Tests/Pods-SecLog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SecLog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SecLog_Tests
+@end
---
+++ Example/Pods/Target Support Files/SecLog/SecLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SecLog : NSObject
+@end
+@implementation PodsDummy_SecLog
+@end
